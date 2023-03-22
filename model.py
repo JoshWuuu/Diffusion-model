ed8970186baea203ed82e8e22be7df8b6fc18e8c
ChunLiang Wu <lucky0978218035@gmail.com>
2023-03-22 15:38:50 -0700

    Update model.py

--- model.py
+++ model.py
@@ -99,7 +99,7 @@ def unit_test_forward():
         image, noise = forward_diffusion_sample(image, t)
         show_tensor_image(image)
 
-class Block(nn.Moudle):
+class Block(nn.Module):
     def __init__(self, in_ch, out_ch, time_emb_dim, up=False):
         super().__init__()
         self.time_mlp =  nn.Linear(time_emb_dim, out_ch)
@@ -143,6 +143,65 @@ class SinusoidalPositionEmbeddings(nn.Module):
         # TODO: Double check the ordering here
         return embeddings
 
+class SimpleUnet(nn.Module):
+    """
+    A simplified variant of the Unet architecture.
+    """
+    def __init__(self):
+        super().__init__()
+        image_channels = 3
+        down_channels = (64, 128, 256, 512, 1024)
+        up_channels = (1024, 512, 256, 128, 64)
+        out_dim = 1 
+        time_emb_dim = 32
+
+        # Time embedding
+        self.time_mlp = nn.Sequential(
+                SinusoidalPositionEmbeddings(time_emb_dim),
+                nn.Linear(time_emb_dim, time_emb_dim),
+                nn.ReLU()
+            )
+        
+        # Initial projection
+        self.conv0 = nn.Conv2d(image_channels, down_channels[0], 3, padding=1)
+
+        # Downsample
+        self.downs = nn.ModuleList(
+            [Block(down_channels[i], down_channels[i+1], time_emb_dim) \
+            for i in range(len(down_channels)-1)]
+        )
+        # Upsample
+        self.ups = nn.ModuleList(
+            [Block(up_channels[i], up_channels[i+1], time_emb_dim, up=True) \
+            for i in range(len(up_channels)-1)]
+        )
+
+        self.output = nn.Conv2d(up_channels[-1], 3, out_dim)
+
+    def forward(self, x, timestep):
+        # Embedd time
+        t = self.time_mlp(timestep)
+        # Initial conv
+        x = self.conv0(x)
+        # Unet
+        residual_inputs = []
+        for down in self.downs:
+            x = down(x, t)
+            residual_inputs.append(x)
+        for up in self.ups:
+            residual_x = residual_inputs.pop()
+            # Add residual x as additional channels
+            x = torch.cat((x, residual_x), dim=1)           
+            x = up(x, t)
+        return self.output(x)
+
+def unit_test_unet():
+    model = SimpleUnet()
+    image = torch.randn(1, 3, 256, 256)
+    timestep = torch.IntTensor([10])
+    output = model(image, timestep)
+    assert output.shape == image.shape, "Output shape does not match input shape"
+    print("Unit test passed")
 
 if __name__ == "__main__":
-    pass
+    unit_test_unet()
